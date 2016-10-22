a1cfea397aea388d8dd5238465ab9cac680f0431
Ajay Raj <Ajay Raj>
2016-10-22 00:53:27 -0700

    Uploaded android application

---
+++ CitrusVote/app/src/main/java/com/example/kenne/myapplication/CreatePoll.java
@@ -0,0 +1,13 @@
+package com.example.kenne.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreatePoll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_poll);
+    }
+}
---
+++ CitrusVote/app/src/main/java/com/example/kenne/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.kenne.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.ViewUtils;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+   public void vote(View view)
+   {
+
+   }
+
+
+}
